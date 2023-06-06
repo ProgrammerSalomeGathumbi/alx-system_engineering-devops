23d71d9e79569d5d1fabfde901a836e7fdec7677
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-06-06 14:52:19 +0300

    Added 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+queries Reddit API and prints  titles of the first 10 hot posts
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    prints titles of first 10 hot posts for a given subreddit
+    """
+    url = 'http://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'tenPosts/0.01 (by GamerG_K)'}
+    params = {'limit': 10}
+    request = requests.get(url, headers=headers, params=params,
+                           allow_redirects=False)
+    if request.status_code == 200:
+        posts = request.json().get("data").get("children")
+        for post in posts:
+            title = post.get("data").get("title")
+            print(title)
+    else:
+        print("None")
