1dd910b17ff37c6a6843a62354ce62373cb66409
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-06-08 11:15:47 +0300

    Fixed the error now top ten displays the top ten posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-queries Reddit API and prints  titles of the first 10 hot posts
+queries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit
 """
+
 import requests
 
 
 def top_ten(subreddit):
     """
-    prints titles of first 10 hot posts for a given subreddit
+    First 10 hot posts in the subreddit
     """
-    url = 'http://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'tenPosts/0.01 (by GamerG_K)'}
-    params = {'limit': 10}
-    request = requests.get(url, headers=headers, params=params,
-                           allow_redirects=False)
-    if request.status_code == 200:
-        data = response.json().get("data")
-        posts = data.get("data").get("children")
-        for post in posts:
-            title = post.get("data").get("title")
-            print(title)
-    else:
-        print("None")
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    header = {'User-Agent': 'topTen/0.0.1(by /u/GamerG_K)'}
+    request = requests.get(url, headers=header, allow_redirects=False)
+    try:
+        posts = request.json().get("data").get("children")
+
+        if request.status_code == 200:
+            for post in posts[:10]:
+                print(post.get('data').get('title'))
+    except Exception:
+        print(None)
