e3fb60a6a121d7c3e2485c5a466834dfbb41b5a8
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-06-14 11:55:56 +0300

    Fixed puppet-lint errors

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # accept more requests
 exec { 'fix web-server':
   command => "sed -i 's/15/4096/' /etc/default/nginx",
-  path    => '/usr/local/bin/:/bin/'	  
+  path    => '/usr/local/bin/:/bin/'
 }
 exec { 'fix -nginx':
   command => "sed -i '6s/768/1024/' /etc/nginx/nginx.conf",
   path    => '/usr/local/bin/:/bin/'
-}	  
-service { 'nginx' :
+}
+service { 'nginx':
   restart  => 'sudo service nginx restart',
-}	  
+}
