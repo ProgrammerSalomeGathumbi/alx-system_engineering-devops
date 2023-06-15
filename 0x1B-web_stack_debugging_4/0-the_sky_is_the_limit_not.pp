499550bc384d6931965e097ce3e8a7454c0fc85d
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-06-15 08:36:03 +0300

    fixed errors

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,10 +3,7 @@ exec { 'fix web-server':
   command => "sed -i 's/15/4096/' /etc/default/nginx",
   path    => '/usr/local/bin/:/bin/'
 }
-exec { 'fix -nginx':
-  command => "sed -i '6s/768/1024/' /etc/nginx/nginx.conf",
-  path    => '/usr/local/bin/:/bin/'
-}
-service { 'nginx':
-  restart  => 'sudo service nginx restart',
+exec { 'restart nginx':
+  command  => 'sudo service nginx restart',
+  path     => '/etc/init.d'	   
 }
